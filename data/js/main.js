71847219d730d7f169d1cde75a214016190af463
huangkevin57 <khuang01@college.harvard.edu>
2017-11-08 16:17:12 -0500

    oops wrong file

--- data/js/main.js
+++ data/js/main.js
@@ -49,11 +49,22 @@ d3.csv("data/zaatari-refugee-camp-population.csv", function(data) {
         .y1(function(d) { return populationScale(d.population); })
         .y0(height);
 
-    var path = svg.append("path")
-        .datum(data)
-        .attr("class", "area")
-        .attr("d", area);
+    var line = d3.line()
+        .x(function(d) {
+            return dateScale(d.date);
+        })
+        .y(function(d) {
+            return populationScale(d.population);
+        })
+        .curve(d3.curveLinear);
 
+    // svg.append("path")
+    //     .datum(data)
+    //     .attr("d", line)
+    //     .attr("stroke-width", 2)
+    //     .attr("stroke", "red")
+    //     .attr("fill", "none")
+    //     .attr("id", "actual");
 
 
     var lineSvg = svg.append("g");
@@ -74,41 +85,154 @@ d3.csv("data/zaatari-refugee-camp-population.csv", function(data) {
     focus.append("text")
         .attr("class", "txtDate");
 
+    var mouse = false;
+    var lastCoordinate = -1;
+    var drawData = [];
+    var displayData = data.slice(0,101);
+    var correctData = data.slice(101);
+    var corrected = false;
+
+    svg.append("path")
+        .datum(displayData)
+        .attr("d", line)
+        .attr("stroke-width", 2)
+        .attr("stroke", "green")
+        .attr("fill", "none")
+        .attr("id", "display");
+
+    // svg.append("path")
+    //     .datum(correctData)
+    //     .attr("d", line)
+    //     .attr("stroke-width", 2)
+    //     .attr("stroke", "red")
+    //     .attr("fill", "none")
+    //     .attr("id", "correct");
+
+    svg.append("path")
+        .datum(drawData)
+        .attr("d", line)
+        .attr("stroke-width", 2)
+        .attr("stroke", "blue")
+        .attr("fill", "none")
+        .attr("id", "user")
+        .attr("stroke-dasharray","5 2");
+
     // append the rectangle to capture mouse
     svg.append("rect")
         .attr("width", width)
         .attr("height", height)
         .style("fill", "none")
         .style("pointer-events", "all")
-        .on("mouseover", function() { console.log("HERE"); focus.style("display", null); })
-        .on("mouseout", function() { console.log("AAA"); focus.style("display", "none"); })
-        .on("mousemove", mousemove);
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mousemove", mouseclick)
+        .on("mousedown", increment)
+        .on("mouseup", decrement)
+        .on("mouseout", decrement);
 
-    function mousemove() {
+    svg.append("text")
+        .attr("x", width - 85)
+        .attr("y", 35)
+        .text("DONE");
+    
+    svg.append("rect")
+        .attr("x", width - 100)
+        .attr("y", 10)
+        .attr("opacity", .7)
+        .attr("width", 70)
+        .attr("height", 40)
+        .style("fill", "red")
+        .on("click", showCorrect);
+
+    function showCorrect() {
+        svg.append("path")
+            .datum(correctData)
+            .attr("d", line)
+            .attr("stroke-width", 2)
+            .attr("stroke", "red")
+            .attr("fill", "none")
+            .attr("id", "correct");
+    }
+    
+    function increment() {
+        mouse = true;
         var x0 = dateScale.invert(d3.mouse(this)[0]),
             i = bisectDate(data, x0, 1),
             d0 = data[i - 1],
             d1 = data[i],
-            d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-
-        focus.select("text.txt")
-            .attr("transform",
-                "translate(" + (dateScale(d.date) + 8) + ",40)")
-            .text(d.population.toLocaleString());
-
-        focus.select("text.txtDate")
-            .attr("transform",
-                "translate(" + (dateScale(d.date) + 8) + ",53)")
-            .text(d.date.getFullYear() +'-' + (d.date.getMonth() + 1) + '-' + d.date.getDate());
-
-        focus.select("line.x")
-            .attr("transform",
-                "translate(" + dateScale(d.date) + ",0)");
-        console.log(dateScale(d.date));
-        console.log(populationScale(d.population));
+            iSelect = x0 - d0.date > d1.date - x0 ? i : i - 1;
+        lastCoordinate = iSelect - 100;
+        console.log("MOUSE PRESSED");
+        
+        // if (!corrected) {
+        //     svg.append("path")
+        //         .datum(correctData)
+        //         .attr("d", line)
+        //         .attr("stroke-width", 2)
+        //         .attr("stroke", "red")
+        //         .attr("fill", "none")
+        //         .attr("id", "correct");
+        //     corrected = true;
+        // }
+    }
+    function decrement() {
+        mouse = false;
+        lastCoordinate = -1;
+        console.log("UP")
     }
 
+    function mouseclick() {
+        // console.log(d3.mouse(this));
+        if (mouse) {
+            var x0 = dateScale.invert(d3.mouse(this)[0]),
+                i = bisectDate(data, x0, 1),
+                d0 = data[i - 1],
+                d1 = data[i],
+                d = x0 - d0.date > d1.date - x0 ? d1 : d0,
+                iSelect = x0 - d0.date > d1.date - x0 ? i : i - 1;
+            // console.log(iSelect);
+            if (iSelect > 100) {
+                var effSelect = iSelect - 100;
+                if (effSelect > drawData.length - 1) {
+                    for (var i = drawData.length; i <= effSelect; i++) {
+                        drawData.push({
+                            date: data[i + 100].date,
+                            population: populationScale.invert(d3.mouse(this)[1])
+                        });
+                    }
+                }
 
+                drawData[effSelect].population = populationScale.invert(d3.mouse(this)[1]);
+
+                console.log(effSelect + ", " + lastCoordinate);
+                if (effSelect < lastCoordinate) {
+                    for (i = effSelect; i < lastCoordinate; i++) {
+                        drawData[i].population = drawData[effSelect].population;
+                    }
+                }
+                if (effSelect > lastCoordinate && lastCoordinate >= 0) {
+                    for (i = lastCoordinate + 1; i < effSelect; i++) {
+                        drawData[i].population = drawData[effSelect].population;
+                    }
+                }
+
+                lastCoordinate = effSelect;
+
+                var rect = svg.select("#user");
+                rect.enter()
+                    .merge(rect)
+                    .attr("d", line);
+                rect.exit().remove();
+
+            }
+
+            // var rect = svg.select("#actual");
+            // rect.enter()
+            //     .merge(rect)
+            //     .attr("d", line);
+            // rect.exit().remove();
+        }
+    }
 
 
 
@@ -146,111 +270,6 @@ d3.csv("data/zaatari-refugee-camp-population.csv", function(data) {
 
 
 
-
-
-
-    var shelter = {};
-    shelter["Caravans"] = .7968;
-    shelter["Tents"] = .0951;
-    shelter["Combination"] = .1081;
-
-    var svg = d3.select("body").select("#right")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var percentageScale = d3.scaleLinear()
-        .domain([0., 1.0])
-        .range([height, 0]);
-
-    var ordinalScale = d3.scaleOrdinal()
-        .domain(['','Caravans', 'Combination', 'Tents',''])
-        .range([0,50, 150, 250, 300]);
-
-    for (var key in shelter) {
-        svg.append("rect")
-            .attr("class", "bars")
-            .attr("x", ordinalScale(key) - 40)
-            .attr("y", percentageScale(shelter[key]))
-            .attr("width", 80)
-            .attr("height", height - percentageScale(shelter[key]));
-
-        svg.append("text")
-            .text((100 * shelter[key]).toFixed(2) + "%")
-            .attr("x", ordinalScale(key))
-            .attr("y", percentageScale(shelter[key]) - 10)
-            .attr("text-anchor", "middle");
-
-    }
-
-    var xAxis = d3.axisBottom()
-        .scale(ordinalScale);
-
-    svg.append("g")
-        .attr("class", "axis x-axis")
-        .attr("transform", "translate(0"  + ',' + height + ")")
-        .call(xAxis)
-        .selectAll("text")
-        .attr("y", 8)
-        .attr("x", 0)
-        .style("text-anchor", "middle");
-
-    var yAxis = d3.axisLeft()
-        .scale(percentageScale)
-        .tickFormat(d3.format(".0%"));
-
-    svg.append("g")
-        .attr("class", "axis y-axis")
-        .attr("transform", "translate(0"  + ", 0)")
-        .call(yAxis);
-
-    // svg.append("rect")
-    //     .attr("x", 50)
-    //     .attr("y", height - percentageScale(shelter["caravans"]))
-    //     .attr("width", 80)
-    //     .attr("height", percentageScale(shelter["caravans"]));
-    //
-    // svg.append("rect")
-    //     .attr("x", 150)
-    //     .attr("y", height - percentageScale(shelter["combination"]))
-    //     .attr("width", 80)
-    //     .attr("height", percentageScale(shelter["combination"]));
-    //
-    // svg.append("rect")
-    //     .attr("x", 250)
-    //     .attr("y", height - percentageScale(shelter["tents"]))
-    //     .attr("width", 80)
-    //     .attr("height", percentageScale(shelter["tents"]));
-
-
-
-    console.log(ordinalScale("caravans"))
-    console.log(ordinalScale("combination"))
-    console.log(ordinalScale("tents"))
-
-    svg.append("text")
-        .text("Type of Shelter")
-        .attr("class", "title")
-        .attr("x", 170)
-        .attr("y", 0);
-
-
-    // svg.selectAll("circle")
-    //     .data(data)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("cx", function(d, index) {
-    //         return dateScale(d.date);
-    //     })
-    //     .attr("cy", function(d, index) {
-    //         return populationScale(d.population);
-    //     })
-    //     .attr("r", 5)
-    //     .attr("stroke", "black");
-    //
-    // console.log(data[5]);
 });
 
 
